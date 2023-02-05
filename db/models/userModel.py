be55cd8b9cafdf77b69d941a4d14a532ea66ea96
Cesar Villanueva <lordchief21@gmail.com>
2023-02-05 00:49:38 -0600

    "Implements configuration for Deta"

--- db/models/userModel.py
+++ db/models/userModel.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class User(BaseModel): #Esto es un BaseModel#
-    id: str | None  #El <<None>> es para determinar que sea opcional que se pase#
+    id: Optional[str]  #El <<None>> es para determinar que sea opcional que se pase y el Optional es porque Deta corre con python 3.9#
     username: str
     email: str
